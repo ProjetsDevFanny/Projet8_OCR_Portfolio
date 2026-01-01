dedffa901f508ad7e843fb62c70cffb9a43b7d2a
Fanny Simon <contact.fanny.simon@gmail.com>
2026-01-01 18:24:14 +0100

    feat(history): history & historyItem components done

--- src/Components/Sections/History/History-Items/HistoryItem.jsx
+++ src/Components/Sections/History/History-Items/HistoryItem.jsx
@@ -10,12 +10,13 @@ import React from 'react'
 import './historyItem.scss'
 
 function HistoryItem({ experience }) {
-  const { title, location, description, date, side } = experience;
+  const { title, entreprise, location, description, date, side } = experience;
 
   return (
     <div className={`timeline-item timeline-item--${side}`}>
       <div className="timeline-item__content">
         <h3 className="timeline-item__title">{title}</h3>
+        <h4 className="timeline-item__entreprise">{entreprise}</h4>
         <span className="timeline-item__location">{location}</span>
         <p className="timeline-item__description">{description}</p>
       </div>
