548e5d268b32376be049524b445114fd147d5c93
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2026-07-01 16:48:59 +0200

    chore(cypress.config) : baseURL configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+  // signifie que Cypress interdit l'utilisation de l'ancienne API Cypress.env() et pousse à utiliser les nouvelles API (cy.env() ou Cypress.expose()) pour gérer les variables d'environnement. C'est une mesure de sécurité et de modernisation. Pour ton portfolio, tu n'utilises probablement aucune variable d'environnement Cypress, donc cette ligne ne va rien changer à tes tests.
 
   e2e: {
+    baseUrl: "http://localhost:3001",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
