715040c4f4aebaaf795f4b6d64abd925feb888e5
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2026-07-02 16:41:45 +0200

    test(homePage.cy) : trying to test fullsize screenshot of my SPA, but it doesn't work well

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   // signifie que Cypress interdit l'utilisation de l'ancienne API Cypress.env() et pousse à utiliser les nouvelles API (cy.env() ou Cypress.expose()) pour gérer les variables d'environnement. C'est une mesure de sécurité et de modernisation. Pour ton portfolio, tu n'utilises probablement aucune variable d'environnement Cypress, donc cette ligne ne va rien changer à tes tests.
 
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrlLocal: "http://localhost:3001",
+    baseUrlProduction: "https://fannysimon-devweb-fullstack.netlify.app",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/e2e/visual/homePage.cy.js
@@ -0,0 +1,61 @@
+describe('Visual comparison - Home Page', () => {
+
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.wait(500);
+  });
+
+  it('Capture the page locally', () => {
+    cy.visit(Cypress.config('baseUrlLocal'));
+
+    // Afficher la hauteur du document et du body (vu par Cypress)
+    cy.document().then((doc) => {
+      console.log("documentElement :", doc.documentElement.scrollHeight);
+      console.log("body :", doc.body.scrollHeight);
+    });
+
+    // Identification de quel élément défile avec le scroll: Si scrollY reste à 0, cela voudra dire que ce n'est pas la fenêtre qui défile
+    cy.window().then((win) => {
+      console.log("Avant scroll :", win.scrollY);
+
+      win.scrollTo(0, 5000);
+
+      console.log("Après scroll :", win.scrollY);
+    });
+
+    cy.scrollTo('bottom');
+    cy.scrollTo('top');
+
+
+    cy.screenshot('home-local', {
+      capture: 'fullPage'
+    });
+  });
+
+  it('Capture the page in production', () => {
+    cy.visit(Cypress.config('baseUrlProduction'));
+
+    // Afficher la hauteur du document et du body (vu par Cypress)
+    cy.document().then((doc) => {
+      console.log("documentElement :", doc.documentElement.scrollHeight);
+      console.log("body :", doc.body.scrollHeight);
+    });
+
+    // Identification de quel élément défile avec le scroll : Si scrollY reste à 0, cela voudra dire que ce n'est pas la fenêtre qui défile
+    cy.window().then((win) => {
+      console.log("Avant scroll :", win.scrollY);
+
+      win.scrollTo(0, 5000);
+
+      console.log("Après scroll :", win.scrollY);
+    });
+
+    cy.scrollTo('bottom');
+    cy.scrollTo('top');
+
+    cy.screenshot('home-prod', {
+      capture: 'fullPage'
+    });
+  });
+
+});
